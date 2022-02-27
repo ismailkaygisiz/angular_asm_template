cb188c16f6f14b59d5df478a2471168aeefca608
ismailkaygisiz <kaygisizismail24@gmail.com>
2022-02-27 13:37:39 +0300

    404 component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { Error404Component } from './components/error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
